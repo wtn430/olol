0859b26f6d3315b41128d45d22737b6c97abf708
zhangsan <zhangsan@itcast.cn>
2019-03-12 11:40:28 +0800

    xiugai

--- untitled/src/com/itheima/pinyougou/dog.java
+++ untitled/src/com/itheima/pinyougou/dog.java
@@ -2,7 +2,8 @@ package com.itheima.pinyougou;
 
 public class dog {
     public static void main(String[] args) {
-        System.out.println("1111111111111");
+
+        System.out.println("1111111111111");   System.out.println("1111111111111");   System.out.println("1111111111111");
     }
 
     public void test (){
